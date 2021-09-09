f85481e62e7a2245ec360ec4fe343a982691762b
G3FRY <gefry666@gmail.com>
2021-09-09 10:35:38 -0500

    Creando vistas y llamando ventana principal desde clase Peliculas

--- Reto_5/src/peliculas/Peliculas.java
+++ Reto_5/src/peliculas/Peliculas.java
@@ -1,5 +1,7 @@
 package peliculas;
 
+import views.MainWindow;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -17,6 +19,7 @@ public class Peliculas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        new MainWindow();
     }
     
 }
---
+++ Reto_5/src/views/ResultsPanel.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package views;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+/**
+ *
+ * @author gefry
+ */
+public class ResultsPanel extends JPanel{
+    
+}
